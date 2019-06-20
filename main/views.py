292108d74cab973dde00f8def8d198d170664fa7
Jesse Mervasto <jmervasto@gmail.com>
2019-06-20 11:55:28 +0300

    update

--- main/views.py
+++ main/views.py
@@ -5,18 +5,22 @@ from .models import *
 
 def index(request):
     main_content = Main.objects.all()
-    latest_entries = Entry.objects.order_by('-date')[0:5]
-    context = {'main_content': main_content, 'latest_entries': latest_entries}
+    latest_entries = Entry.objects.order_by('-date')[0:10]
+    categories = Category.objects.all()
+    context = {'main_content': main_content, 'latest_entries': latest_entries,
+               'categories': categories,}
     return render(request, 'main/index.html', context)
 
 def entries(request):
+    categories = Category.objects.all()
     entries = Entry.objects.order_by('-date')
-    context = {'entries': entries}
+    context = {'categories': categories, 'entries': entries}
     return render(request, 'main/entries.html', context)
 
 def entry(request, entry_id):
+    categories = Category.objects.all()
     entry = Entry.objects.get(id=entry_id)
-    context = {'entry': entry}
+    context = {'categories': categories, 'entry': entry}
     return render(request, 'main/entry.html', context)
 
 def categories(request):
@@ -25,7 +29,8 @@ def categories(request):
     return render(request, 'main/categories.html', context)
 
 def category(request, category_id):
+    categories = Category.objects.all()
     category = Category.objects.get(id=category_id)
     entries = category.entry_set.order_by('-date')
-    context = {'category': category, 'entries': entries}
+    context = {'categories': categories, 'category': category, 'entries': entries}
     return render(request, 'main/category.html', context)
